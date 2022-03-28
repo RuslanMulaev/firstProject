71432957f9d7557b4a0fa2dc5a26ac7822561d07
Eldar Toleubay <eldartoleubay@gmail.com>
2022-03-28 13:22:21 +0600

    added new method modulo

--- JavaMathOperations/src/Main.java
+++ JavaMathOperations/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
-    public static int(int a, int b){
-	return a*b;
+    public static int modulo(int a, int b){
+	return a%b;
     }
 
     public static void main(String[] args) {
