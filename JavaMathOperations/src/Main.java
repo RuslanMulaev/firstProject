aef4453c43d3d9343b36dfc2aa89f3d8c43e8a42
zhassurbek <zhassurbek.boltabayev.2000@gmail.com>
2022-03-28 14:00:11 +0600

    zhassurbek

--- JavaMathOperations/src/Main.java
+++ JavaMathOperations/src/Main.java
@@ -25,6 +25,7 @@ public class Main {
 //        System.out.println(a+b);
         System.out.println(a*b);
         /////Alnur
+        // zhassurbek
 
         // -
         System.out.println(a-b);
