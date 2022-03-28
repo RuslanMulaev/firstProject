de01e8cbfa5796ff59b6fc7caf1657c13f6e1cef
Eldar Toleubay <eldartoleubay@gmail.com>
2022-03-28 15:23:12 +0600

    changed code of multiply operation to method

--- JavaMathOperations/src/Main.java
+++ JavaMathOperations/src/Main.java
@@ -1,18 +1,20 @@
 import java.util.Scanner;
 
 public class Main {
-    public static int modulo(int a, int b){
-	return a%b;
+    public static int modulo(int a, int b) {
+        return a % b;
     }
 
-    public static int sum(int a, int b){
-        return a+b;
+    public static int sum(int a, int b) {
+        return a + b;
     }
 
 
-    public static int substraction(int a, int b)
-    {
+    public static int substraction(int a, int b) {
         return a - b;
+    }
+    public static int multiply(int a,int b){
+        return a*b;
     }
 
     public static void main(String[] args) {
@@ -21,11 +23,10 @@ public class Main {
         int a = in.nextInt();
         System.out.print("b: ");
         int b = in.nextInt();
-        sum(a, b);
-//        System.out.println(a+b);
-        System.out.println(a*b);
 
-        // -
-        System.out.println(a-b);
+        System.out.println("a+b="+sum(a,b));
+        System.out.println("a-b="+substraction(a,b));
+        System.out.println("a*b="+multiply(a,b));
+        System.out.println("a%b="+modulo(a,b));
     }
 }
