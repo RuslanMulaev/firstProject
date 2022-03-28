ce265162265fcb38aab2bb946c303767007cdb29
zhassurbek <zhassurbek.boltabayev.2000@gmail.com>
2022-03-28 12:10:08 +0600

    Добавил 1 строку

--- firstProject.java
+++ firstProject.java
@@ -3,5 +3,6 @@ public class firstProject {
 	String str1 = "!";
         System.out.println("Hello world+str1");
         System.out.println("Не трогайте файлам!!!");
+        System.out.println("Парни, давайте за дело, времю теряем!");
     }
 }
