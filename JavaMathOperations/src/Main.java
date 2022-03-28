e4c5c5b26f8e59165d765ad68596737193b97d56
Eldar Toleubay <eldartoleubay@gmail.com>
2022-03-28 15:34:01 +0600

    added new method pow

--- JavaMathOperations/src/Main.java
+++ JavaMathOperations/src/Main.java
@@ -4,17 +4,21 @@ public class Main {
     public static int modulo(int a, int b) {
         return a % b;
     }
-
     public static int sum(int a, int b) {
         return a + b;
     }
-
-
     public static int substraction(int a, int b) {
         return a - b;
     }
     public static int multiply(int a,int b){
         return a*b;
+    }
+    public static int pow(int a,int b){
+        int sum = 1;
+        for (int i = 0; i < b; i++) {
+            sum*=a;
+        }
+        return sum;
     }
 
     public static void main(String[] args) {
@@ -28,5 +32,6 @@ public class Main {
         System.out.println("a-b="+substraction(a,b));
         System.out.println("a*b="+multiply(a,b));
         System.out.println("a%b="+modulo(a,b));
+        System.out.println("a^b="+pow(a,b));
     }
 }
