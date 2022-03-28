c32565275827feebeb1577e0bec20ea3c694fc6e
Eldar Toleubay <eldartoleubay@gmail.com>
2022-03-28 13:44:33 +0600

    Update Main.java

--- JavaMathOperations/src/Main.java
+++ JavaMathOperations/src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
-    public static int modulo(int a, int b){
-	return a%b;
+    public static int modulo(int a, int b) {
+        return a % b;
     }
 
     public static void main(String[] args) {
         int a = 5;
         int b = 4;
-	modulo(a,b);
-        System.out.println(a+b);
-        System.out.println(a*b);
+        modulo(a, b);
+        System.out.println(a + b);
+        System.out.println(a * b);
     }
 }
