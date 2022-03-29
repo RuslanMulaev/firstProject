71df5f62b0a1768f6cf84c8a1ba142eae71f48db
RuslanMulaev <rusmulaev123@gmail.com>
2022-03-29 11:58:05 +0600

    Hurfood

--- JavaMathOperations/src/Gluu.java
+++ JavaMathOperations/src/Gluu.java
@@ -4,10 +4,6 @@ public class Gluu {
         System.out.println("Hello everybody");
 
         System.out.println("hello");
-<<<<<<< HEAD
-        System.out.println("hiii");
-=======
         System.out.println("Eldar");
->>>>>>> 8bdc3c6beac159285e04275721056065a81c6efe
     }
 }
