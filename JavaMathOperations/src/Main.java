d1c709e086bb70e6f0f6d70f4bf113dcd3fbeadd
Eldar Toleubay <eldartoleubay@gmail.com>
2022-03-29 09:09:02 +0600

    changed code style and added coments for discription

--- JavaMathOperations/src/Main.java
+++ JavaMathOperations/src/Main.java
@@ -6,28 +6,32 @@ public class Main {
     public static int modulo(int a, int b) {
         return a % b;
     }
+
     //метод вычясление суммы
     public static int sum(int a, int b) {
         return a + b;
     }
+
     //метод минус
     public static int substraction(int a, int b) {
         return a - b;
     }
+
     //метод умножения двух чисел
-    public static int multiply(int a,int b){
-        return a*b;
+    public static int multiply(int a, int b) {
+        return a * b;
     }
+
     //метод степени аналог (Math.pow)
-    public static int pow(int a,int b){
+    public static int pow(int a, int b) {
         int sum = 1;
         for (int i = 0; i < b; i++) {
-            sum*=a;
+            sum *= a;
         }
         return sum;
     }
 
-    
+
     public static void main(String[] args) {
         //создаем новый сканнер
         Scanner in = new Scanner(System.in);
@@ -38,10 +42,10 @@ public class Main {
         int b = in.nextInt();
 
         //вызов методов
-        System.out.println("a+b="+sum(a,b));
-        System.out.println("a-b="+substraction(a,b));
-        System.out.println("a*b="+multiply(a,b));
-        System.out.println("a%b="+modulo(a,b));
-        System.out.println("a^b="+pow(a,b));
+        System.out.println("a + b = " + sum(a, b));
+        System.out.println("a - b = " + substraction(a, b));
+        System.out.println("a * b = " + multiply(a, b));
+        System.out.println("a % b = " + modulo(a, b));
+        System.out.println("a ^ b = " + pow(a, b));
     }
 }
