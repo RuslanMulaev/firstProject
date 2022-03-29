69e5a44e6c95678cb43bc4764ff627b32b437441
Sakayev Alnur <alnursakaev@gmail.com>
2022-03-29 11:54:33 +0600

    sadasdasdas

--- JavaMathOperations/src/Gluu.java
+++ JavaMathOperations/src/Gluu.java
@@ -1,14 +1,6 @@
 public class Gluu {
     public static void main(String[] args) {
-        System.out.println(1+1);
-        System.out.println(2+2);
 
-        System.out.println("ufyguygu");
-        for (int i = 0; i < 2343; i++) {
-            for (int j = 0; j < i; j++) {
-                System.out.println(i);
-            }
-        }
 
     }
 }
