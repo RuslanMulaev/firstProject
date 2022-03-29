ad4f2e6f7943132a81fae0fa18963f98fd82deb6
Eldar Toleubay <eldartoleubay@gmail.com>
2022-03-29 11:13:00 +0600

    added 3 for loop

--- JavaMathOperations/src/Main.java
+++ JavaMathOperations/src/Main.java
@@ -59,7 +59,9 @@ public class Main {
 
         for (int i = 0; i < 1; i++) {
             for (int j = 0; j < 5; j++) {
+                for (int k = 0; k < 32; k++) {
 
+                }
             }
         }
     }
